0acfe3b0b9f0dd516c113f961a453330a9d66cea
Kos <koshuang@gmail.com>
2021-05-31 22:33:20 +0800

    init

---
+++ src/routes.ts
@@ -0,0 +1,17 @@
+import getDebugger from 'debug';
+const debug = getDebugger('conversion-ai-scraper:routes');
+
+export const handleStart = async ({ request, page }) => {
+  // Handle Start URLs
+
+  debug('handle start!!');
+};
+
+export const handleList = async ({ request, page }) => {
+  // Handle pagination
+  debug('handle list');
+};
+
+export const handleDetail = async ({ request, page }) => {
+  // Handle details
+};
